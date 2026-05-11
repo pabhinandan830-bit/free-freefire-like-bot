4f45f2edffbde0640a711d6a8096c866ca2ee642
pabhinandan830-bit <pabhinandan830@gmail.com>
2026-05-10 16:51:01 -0800

    Update likeCommands.py

--- cogs/likeCommands.py
+++ cogs/likeCommands.py
@@ -15,7 +15,7 @@ CONFIG_FILE = "like_channels.json"
 class LikeCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_host = "https://likeapixthug.onrender.com"
+        self.api_host = "http://like.thug4ff.xyz/like?uid={uid}&server_name={region}&key=abhi45"
         self.config_data = self.load_config()
         self.cooldowns = {}
         self.session = aiohttp.ClientSession()
@@ -144,8 +144,8 @@ class LikeCommands(commands.Cog):
                     else:
                         embed.description = "\n┌MAX LIKES\n└─This UID has already received the maximum likes today.\n"
 
-                    embed.set_footer(text="DEVELOPED BY THUG")
-                    embed.description += "\n🔗 JOIN : https://discord.gg/awPm5B3QFg"
+                    embed.set_footer(text="DEVELOPED BY ABHINANDAN")
+                    embed.description += "\n🔗 JOIN : https://discord.gg/GmuctQ8m"
                     await ctx.send(embed=embed, mention_author=True, ephemeral=is_slash)
 
         except asyncio.TimeoutError:
